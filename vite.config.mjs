52c919e3d090b738b3d1f184a5c8fe5a98722bba
Petter Andersson <petter.andersson@helsingborg.se>
2025-11-10 13:40:01 +0100

    fix path

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,11 @@
-import { createViteConfig } from 'vite-config-factory'
+import { createViteConfig } from "vite-config-factory";
 
 const entries = {
-    'js/acf-ux-collapse':  './source/js/FieldTypes/Repeater.ts',
-    'css/acf-ux-collapse': './source/sass/acf-ux-collapse.scss',
-}
+	"js/acf-ux-collapse": "./source/js/FieldTypes/Repeater.ts",
+	"css/acf-ux-collapse": "./source/sass/acf-ux-collapse.scss",
+};
 
 export default createViteConfig(entries, {
-    outDir: 'dist',
-    manifestFile: 'manifest.json',
-})
+	outDir: "assets/dist",
+	manifestFile: "manifest.json",
+});
